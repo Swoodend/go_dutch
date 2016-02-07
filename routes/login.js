48ee2d1726b78735abe8dec93459224eb177681d
Scott Woodend <woodendscott@gmail.com>
2016-02-07 14:46:53 -0500

    add login functionality

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var userModel = require('../models/users');
+var User = mongoose.model('User');
 
 router.get('/', function(req, res){
   res.render('login');
@@ -8,7 +11,21 @@ router.get('/', function(req, res){
 router.post('/', function(req, res){
   // find user in databse, verify password, set session
   //check invites middleware if login successful
-  res.redirect('dashboard');
+  
+  User.findOne({ email: req.body.email }, function(err, user) {
+    if (!user){
+      var error = 'No user found with that email. Head to registration page.';
+      res.render('login', { error: error});
+    } else {
+      console.log(user.password)
+      if (user.password === req.body.password){
+        res.redirect('dashboard');
+      } else{
+        console.log(user.password)
+        res.render('login', {error: 'Incorrect password. Try again'});
+      }
+    }
+  });
 });
 
 

69e2b05bd458f72c30f28118baf796565362f7b0
Scott Woodend <woodendscott@gmail.com>
2016-02-11 23:35:20 -0500

    important comment! dont forget

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -13,6 +13,8 @@ function checkSession(req,res,next){
 }
 
 router.get('/', checkSession, function(req, res){
+  // actually find the user obj in the database and pass that in to the template
+  // this will allow you to display the room info of the user :)
   res.render('../views/dashboard', {user: req.session.user});
 });
 

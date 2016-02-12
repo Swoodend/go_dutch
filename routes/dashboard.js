79c0d532508898835d12f79a15b1de2b07b2ca52
Scott Woodend <woodendscott@gmail.com>
2016-02-12 17:41:36 -0500

    pass actuall user object to dashboard template to extract all available info on user

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -13,9 +13,13 @@ function checkSession(req,res,next){
 }
 
 router.get('/', checkSession, function(req, res){
-  // actually find the user obj in the database and pass that in to the template
-  // this will allow you to display the room info of the user :)
-  res.render('../views/dashboard', {user: req.session.user});
+  var user = User.findOne({email: req.session.user.email}, function(err, user){
+    if (user){
+      res.render('../views/dashboard', {user: user});
+    } else{
+      res.send('something went wrong');
+    }
+  });
 });
 
 
